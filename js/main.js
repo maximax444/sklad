3aca911adba9e5e443857a4eb5ff24c2ffb0799a
Max <max.kuzn.msx@gmail.com>
2022-09-18 21:14:36 +0300

    desktop vers of home-page

--- js/main.js
+++ js/main.js
@@ -19,3 +19,39 @@ $('.home-qa__name').on('click', function () {
 
     });
 })(jQuery);
+(function ($) {
+    $(function () {
+
+        $('.home-reviews__tabs').on('click', 'a:not(.active)', function (e) {
+            e.preventDefault();
+
+            $(this).closest('.home-reviews').find('.home-reviews__cont-block').removeClass('active').eq($(this).index()).find('.frame__wrap').sly('init');
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('.home-reviews').find('.home-reviews__cont-block').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+    });
+})(jQuery);
+var $frame = $('.frame__wrap');
+var $slidee = $('.frame');
+var $wrap = $frame.parent();
+$frame.each(function () {
+    $(this).sly({
+        horizontal: 1,
+        itemNav: 'basic',
+        smart: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 0,
+        scrollBy: 1,
+        speed: 3000,
+        elasticBounds: 1,
+        easing: 'easeOutExpo',
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1
+    });
+});
